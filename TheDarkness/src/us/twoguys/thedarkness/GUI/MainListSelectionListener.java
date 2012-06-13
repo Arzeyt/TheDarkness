044f592cecf0b7ae3d66ae02e7ccac1fd209911f
Nicholas Edenhoffer <nedenhoffer@gmail.com>
2012-06-13 18:16:33 -0500

    Progress in GUI

---
+++ TheDarkness/src/us/twoguys/thedarkness/GUI/MainListSelectionListener.java
@@ -0,0 +1,60 @@
+package us.twoguys.thedarkness.GUI;
+
+import java.awt.Dimension;
+
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class MainListSelectionListener extends TheDarknessGUI implements ListSelectionListener {
+
+	TheDarknessGUI instance;
+	
+	public MainListSelectionListener(TheDarknessGUI instance){
+		this.instance = instance;
+	}
+	
+	@Override
+	public void valueChanged(ListSelectionEvent e){
+		
+		if(instance.mainList.getSelectedIndex() == 0){
+			subCategoryList = new JList(this.effects);
+			System.out.println("effects");
+			
+		}else if(instance.mainList.getSelectedIndex() == 1){
+			subCategoryList = new JList(this.mobs);
+			System.out.println("mobs clicked");
+			
+		}else if(instance.mainList.getSelectedIndex()==2){
+			subCategoryList = new JList(this.mirages);
+			System.out.println("Mirages Clicked");
+		}
+		
+		//subCategoryList.setLocation(160, 30);
+		//subCategoryList.setSize(150, 50);
+		subCategoryList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		subCategoryList.setLayoutOrientation(JList.VERTICAL);
+		subCategoryList.addListSelectionListener(this);
+		
+		try{
+			instance.selectPanel.remove(instance.subScrollPane);
+		}catch(Exception e1){}
+		
+	    instance.subScrollPane = new JScrollPane(subCategoryList);
+	    instance.subScrollPane.setLocation(160,30);
+	    instance.subScrollPane.setSize(150, 100);
+	    instance.selectPanel.add(instance.subScrollPane);
+	    
+		/*
+		JScrollPane subScrollPane = new JScrollPane(subCategoryList);
+		subScrollPane.setLocation(160,30);
+		subScrollPane.setSize(150,100);
+		*/
+		
+	    instance.selectPanel.updateUI();
+	
+	}
+
+}
--- TheDarkness/src/us/twoguys/thedarkness/GUI/TheDarknessGUI.java
+++ TheDarkness/src/us/twoguys/thedarkness/GUI/TheDarknessGUI.java
@@ -1,14 +1,17 @@
 package us.twoguys.thedarkness.GUI;
 
 	import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 	import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 	import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-	public class TheDarknessGUI implements  ActionListener{
+	public class TheDarknessGUI{
 
 	    // Definition of global values and items that are part of the GUI.
 	int redScoreAmount = 0;
@@ -17,13 +20,20 @@ import java.awt.event.ActionEvent;
 	static int maxX=600;
 	static int maxY=600;
 	
-	JPanel titlePanel, selectPanel, scorePanel, buttonPanel, parameterPanel;
-	JLabel firstLabel, blueLabel, redScore, blueScore;
-	JButton redButton, blueButton, resetButton;
-	JList mainList;
+	JPanel titlePanel, selectPanel, parameterPanel, subCategoryPanel;
+	JLabel firstLabel;
+	JList mainList, subCategoryList;
+	JComboBox subCategoryBox;
+	JScrollPane subScrollPane;
 	
-	String[] main = new String[]{"Effects", "Mobs", "Mirages", "Custom Mirages"};
-	//commit test
+	String[] main = new String[]{"Effects", "Mobs", "Mirages"};
+	String[] effects = new String[]{"Life Drain","Potion", "Precipitation", "Time", "Toch Consume"};
+	String[] mobs = new String[]{"Chicken", "Cow", "Mooshroom", "Ocelot", "Pig", "Sheep", "Squid","Villager","Endermen",
+			"Wolf", "Zombie Pigman", "Blaze", "Cave Spider", "Creeper", "Ghast", "Magma Cube", "Silver Fish", "Skeleton",
+			"Slime", "Spider", "Zombie", "Snow Golem", "Iron Golem"};
+	String[] mirages = new String[]{"Spread"};
+	String[] customMirages;
+	
 	
 	public JPanel createContentPane(){
 	
@@ -48,17 +58,21 @@ import java.awt.event.ActionEvent;
 	    //List panel
 	    selectPanel = new JPanel();
 	    selectPanel.setLayout(null);
-	    selectPanel.setLocation(0,10);
+	    selectPanel.setLocation(0,22);
 	    selectPanel.setSize(maxX,170);
 	    totalGUI.add(selectPanel);
 	    
 	    mainList = new JList(main);
-	    mainList.setLocation(10,30);
-	    mainList.setSize(maxX/4, 100);
 	    mainList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 	    mainList.setLayoutOrientation(JList.VERTICAL);
-	    selectPanel.add(mainList);
-	    
+	    mainList.addListSelectionListener(new MainListSelectionListener(this));
+		
+		JScrollPane mainScrollPane = new JScrollPane(mainList);
+		mainScrollPane.setLocation(10,30);
+		mainScrollPane.setSize(140,100);
+		selectPanel.add(mainScrollPane);
+
+		
 	    //parameter panel
 	    parameterPanel = new JPanel();
 	    parameterPanel.setLayout(null);
@@ -66,92 +80,10 @@ import java.awt.event.ActionEvent;
 	    parameterPanel.setSize(maxX,30);
 	    totalGUI.add(parameterPanel);
 	    
-	    blueLabel = new JLabel("Parameters");
-	    blueLabel.setLocation(0, 300);
-	    blueLabel.setSize(300, 30);
-	    blueLabel.setHorizontalAlignment(0);
-	    blueLabel.setForeground(Color.blue);
-	    parameterPanel.add(blueLabel);
-	
-	    // score panel
-	    scorePanel = new JPanel();
-	    scorePanel.setLayout(null);
-	    scorePanel.setLocation(10, 40);
-	    scorePanel.setSize(260, 30);
-	   // totalGUI.add(scorePanel);
-	
-	    redScore = new JLabel(""+redScoreAmount);
-	    redScore.setLocation(0, 0);
-	    redScore.setSize(120, 30);
-	    redScore.setHorizontalAlignment(0);
-	    scorePanel.add(redScore);
-	
-	    blueScore = new JLabel(""+blueScoreAmount);
-	    blueScore.setLocation(130, 0);
-	    blueScore.setSize(120, 30);
-	    blueScore.setHorizontalAlignment(0);
-	    scorePanel.add(blueScore);
-	
-	    // Creation of a Panel to contain all the JButtons.
-	    buttonPanel = new JPanel();
-	    buttonPanel.setLayout(null);
-	    buttonPanel.setLocation(10, 80);
-	    buttonPanel.setSize(260, 70);
-	   // totalGUI.add(buttonPanel);
-	
-	    // We create a button and manipulate it using the syntax we have
-	    // used before. Now each button has an ActionListener which posts 
-	    // its action out when the button is pressed.
-	    
-	    redButton = new JButton("Red Score!");
-	    redButton.setLocation(0, 0);
-	    redButton.setSize(120, 30);
-	    redButton.addActionListener(this);
-	    buttonPanel.add(redButton);
-	
-	    blueButton = new JButton("Blue Score!");
-	    blueButton.setLocation(130, 0);
-	    blueButton.setSize(120, 30);
-	    blueButton.addActionListener(this);
-	    buttonPanel.add(blueButton);
-	
-	    resetButton = new JButton("Reset Score");
-	    resetButton.setLocation(0, 40);
-	    resetButton.setSize(250, 30);
-	    resetButton.addActionListener(this);
-	    buttonPanel.add(resetButton);
-	    
 	    totalGUI.setOpaque(true);
 	    return totalGUI;
 	}
 	
-	// This is the new ActionPerformed Method.
-	// It catches any events with an ActionListener attached.
-	// Using an if statement, we can determine which button was pressed
-	// and change the appropriate values in our GUI.
-	public void actionPerformed(ActionEvent e) {
-	    if(e.getSource() == redButton)
-	    {
-	        redScoreAmount = redScoreAmount + 1;
-	        redScore.setText(""+redScoreAmount);
-	    }
-	    else if(e.getSource() == blueButton)
-	    {
-	        blueScoreAmount = blueScoreAmount + 1;
-	        blueScore.setText(""+blueScoreAmount);
-	    }
-	    else if(e.getSource() == resetButton)
-	    {
-	        redScoreAmount = 0;
-	        blueScoreAmount = 0;
-	        redScore.setText(""+redScoreAmount);
-	        blueScore.setText(""+blueScoreAmount);
-	    }
-	}
-	
-	public void listSelection(ListSelectionEvent e){
-		//if(e.)
-	}
 	public static void main(String[] args) {
 	    //Schedule a job for the event-dispatching thread:
 	    //creating and showing this application's GUI.
